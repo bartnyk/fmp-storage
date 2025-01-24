b10daf4c968896e160fe73feb05c311381c5b66b
bartnyk <kuba.bartnik114@gmail.com>
2025-01-24 20:25:38 +0100

    fixed app.py

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+import asyncio
 
-app = FastAPI()
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+from cli import get_latest_forex_data, get_latest_forex_events
+
+scheduler = BackgroundScheduler()
+trigger = IntervalTrigger(minutes=10)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/health-check")
-async def health_check():
-    return {"message": "OK"}
+if __name__ == "__main__":
+    scheduler.add_job(lambda: asyncio.run(get_latest_forex_data()), trigger, max_instances=1, replace_existing=True)
+    scheduler.add_job(
+        lambda: asyncio.run(get_latest_forex_events(gui=False)), trigger, max_instances=1, replace_existing=True
+    )
+    scheduler.start()
+    try:
+        while True:
+            ...
+    except:
+        scheduler.shutdown()
